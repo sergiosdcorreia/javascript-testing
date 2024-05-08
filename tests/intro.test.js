6963b054c0f71c1535fa999529b5f9445bbb1cff
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-05-08 18:41:25 +0100

    testing the max and the fizzBuzz functions

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { max, fizzBuzz } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -16,5 +16,23 @@ describe("max", () => {
 
     // Cleaner test - One line of code
     expect(max(2, 1)).toBe(2);
+  });
+  it("should return the second argument if it is greater", () => {
+    expect(max(1, 2)).toBe(2);
+  });
+  it("should return the first argument if arguments are equal", () => {
+    expect(max(1, 1)).toBe(1);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should return FizzBuzz if arg is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("should return Fizz if arg is only divisible by 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+  it("should return Buzz if arg is only divisible by 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
   });
 });
